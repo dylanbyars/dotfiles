4563fe735352c6fa91e238d73554a3d1ab6ba9c6
nnb479 <Dylan.Byars@capitalone.com>
2021-07-27 12:15:08 -0400

    use lsp-signature for signature help. TODO: can it share window settings with lspsaga while maintaining param highlights?

--- nvim/.config/nvim/lua/general.lua
+++ nvim/.config/nvim/lua/general.lua
@@ -209,6 +209,7 @@ require'lspconfig'.vimls.setup{}
 --------------------------
 local saga = require 'lspsaga'
 saga.init_lsp_saga {
+  use_saga_diagnostic_sign = false,
   border_style = 'round',
   max_preview_lines = 15,
   finder_action_keys = {
@@ -222,7 +223,6 @@ saga.init_lsp_saga {
 }
 
 map('n', '<leader>d', '<cmd>:Lspsaga hover_doc<CR>', default_opts)
-map('n', 'gs', '<cmd>:Lspsaga signature_help<CR>', default_opts)
 map('n', 'gr', '<cmd>:Lspsaga rename<CR>', default_opts)
 -- find cursor word definition and references
 map('n', 'gh', '<cmd>:Lspsaga lsp_finder<CR>', default_opts)
@@ -238,7 +238,24 @@ map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', default_opts)
 --------------------------
 require('nvim-autopairs').setup()
 
-require('lsp_signature').setup()
+require('lsp_signature').setup({
+  bind = true, -- This is mandatory, otherwise border config won't get registered.
+  doc_lines = 2, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
+  -- set to 0 if you DO NOT want any API comments be shown
+  -- This setting only take effect in insert mode, it does not affect signature help in normal
+  -- mode, 10 by default
+
+  floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+  fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
+  -- use_lspsaga = false,  -- set to true if you want to use lspsaga popup
+  hi_parameter = "Search", -- how your parameter will be highlight
+  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+  -- to view the hiding contents
+  max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+  handler_opts = {
+    border = "single"   -- double, single, shadow, none
+  },
+})
 
 --------------------------
 -- Compe setup
