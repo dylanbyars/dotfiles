561a0b814616ead08a4baa6f500f751ffc65cd7c
nnb479 <Dylan.Byars@capitalone.com>
2021-07-27 09:36:11 -0400

    notes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,8 +7,7 @@ require 'general'
 require 'statusline'
 
 -- TODO:
--- find (or make) a plugin that shows breadcrumbs to cursor's position like VS Code. tree-sitter should be able to do this
--- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
--- also, look into this and the feature below it: https://github.com/nvim-treesitter/nvim-treesitter#indentation
---
 -- learn more about telescope's built ins
+--
+-- would I use this?
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
