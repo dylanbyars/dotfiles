326265d4de2ee11622a9f818c0066799dfdf566e
nnb479 <Dylan.Byars@capitalone.com>
2021-09-08 14:38:00 -0400

    install packer if it doesn't exist AND run `PackerCompile` everytime the plugin file is saved

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,12 @@
--- plugin repos cloned to ~/.local/share/nvim/site/pack/packer/start/
+-- Install packer
+local execute = vim.api.nvim_command
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+end
+
+vim.cmd [[packadd packer.nvim]] -- see this line in a lot of setup packer automatically snippets. why?
 require('packer').startup({function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -91,6 +99,13 @@ config = {
     end,
   }
 }})
+
+vim.api.nvim_exec([[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]], false)
 
 require('plugins.treesitter')
 
