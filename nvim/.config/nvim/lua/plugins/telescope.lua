1d67bba04f1cc4532ebe28114c8f58f8b2c86a59
nnb479 <Dylan.Byars@capitalone.com>
2021-09-08 23:07:49 -0400

    telescope keymappings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,13 +3,53 @@
 --------------------------
 local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
+local actions = require('telescope.actions')
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ["<c-q>"] = trouble.open_with_trouble },
+      i = {
+        ["<C-c>"] = actions.close,
+        ["<CR>"] = actions.select_default,
+        ["<C-k>"] = actions.preview_scrolling_up,
+        ["<C-j>"] = actions.preview_scrolling_down,
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<C-q>"] = trouble.open_with_trouble,
+        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+
+        ["<C-n>"] = false,
+        ["<C-p>"] = false,
+        ["<Down>"] = false,
+        ["<Up>"] = false,
+        ["<C-x>"] = false,
+        ["<C-v>"] = false,
+        ["<C-t>"] = false,
+        ["<M-q>"] = false,
+        ["<C-l>"] = false,
+      },
+
       n = {
-        ["<c-q>"] = trouble.open_with_trouble,
+        ["<esc>"] = actions.close,
+        ["<CR>"] = actions.select_default,
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+        ["<C-q>"] = trouble.open_with_trouble,
+        ["j"] = actions.move_selection_next,
+        ["k"] = actions.move_selection_previous,
+        ["H"] = actions.move_to_top,
+        ["M"] = actions.move_to_middle,
+        ["L"] = actions.move_to_bottom,
+        ["K"] = actions.preview_scrolling_up,
+        ["J"] = actions.preview_scrolling_down,
+        ["?"] = actions.which_key,
+
+        ["<C-x>"] = false,
+        ["<C-v>"] = false,
+        ["<C-t>"] = false,
+        ["<M-q>"] = false,
+        ["<Down>"] = false,
+        ["<Up>"] = false,
       },
     },
     dynamic_preview_title = true,
