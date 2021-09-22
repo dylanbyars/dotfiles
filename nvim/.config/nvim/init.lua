894d0ad230d61f8856f91701be947ff7eb1153c6
Dylan Byars <dylan@dylanbyars.com>
2021-09-22 12:33:48 -0400

    no more virtual text on problematic lines!

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -217,6 +217,7 @@ map('n', '<leader>o', cmd([[ :OrganizeImports ]]))
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
 
 map('n', 'K', cmd([[ :lua vim.lsp.buf.hover() ]]))
 map('n', '<C-k>', cmd([[ :lua vim.lsp.buf.signature_help() ]]))
