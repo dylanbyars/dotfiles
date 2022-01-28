18836ed14a180369850a6c2dfb12c0629bc66617
dylanbyars <dylan@dylanbyars.com>
2022-01-28 15:17:28 -0500

    let git info in lualine define its own colors + stylua

--- config/.config/nvim/lua/plugins/lualine.lua
+++ config/.config/nvim/lua/plugins/lualine.lua
@@ -1,46 +1,36 @@
-local colors = require('colors')
+local colors = require("colors")
 
 local gps = require("nvim-gps")
 gps.setup()
 
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'tokyonight',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {
-      {'filename', path = 1} -- 0 = just filename, 1 = relative path, 2 = absolute path
-    },
-    lualine_c = {
-      { gps.get_location, condition = gps.is_available },
-    },
-    lualine_x = {
-      {
-        'diff',
-        colored = true, -- displays diff status in color if set to true
-        -- all colors are in format #rrggbb
-        color_added = colors.green, -- changes diff's added foreground color
-        color_modified = colors.yellow, -- changes diff's modified foreground color
-        color_removed = colors.red, -- changes diff's removed foreground color
-        symbols = {added = '+', modified = '~', removed = '-'} -- changes diff symbols
-      }
-    },
-    lualine_y = {'branch'},
-    lualine_z = {'progress'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "tokyonight",
+		component_separators = { "", "" },
+		section_separators = { "", "" },
+		disabled_filetypes = {},
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = {
+			{ "filename", path = 1 }, -- 0 = just filename, 1 = relative path, 2 = absolute path
+		},
+		lualine_c = {
+			{ gps.get_location, condition = gps.is_available },
+		},
+		lualine_x = { { "diff", colored = true } },
+		lualine_y = { "branch" },
+		lualine_z = { "progress" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
