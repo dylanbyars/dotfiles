95e32d1032a69847dbb9d4e26190a04f334366cc
dylanbyars <dylan@dylanbyars.com>
2022-04-15 12:02:43 -0400

    format tabstatusline

--- config/.config/nvim/lua/plugins/tabstatusline.lua
+++ config/.config/nvim/lua/plugins/tabstatusline.lua
@@ -60,42 +60,42 @@ local function tab_label(tabid, active)
 	-- local icon = active and "" or ""
 	local name = util.get_tab_name(tabid)
 	-- return string.format(" %s %s ", icon, name)
-  return string.format("  %s  ", name)
+	return string.format(" %s ", name)
 end
 
 local tabline = {
-		hl = "TabLineFill",
-		layout = "tab_only",
-		-- head = {
-		-- 	{ "  ", hl = { fg = hl_tabline.fg, bg = hl_tabline.bg } },
-		-- 	{ "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
-		-- },
-		-- tail = {
-      -- TODO: put git stuff up here
-		-- 	{ require('gitsigns'), hl = { fg = hl_tabline.fg, bg = hl_tabline.bg } },
-		-- 	{ "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
-		-- },
-		active_tab = {
-			label = function(tabid)
-				return {
-					tab_label(tabid, true),
-					hl = { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, style = "bold" },
-				}
-			end,
-			left_sep = {"" , hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
-			right_sep = { "", hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
-		},
-		inactive_tab = {
-			label = function(tabid)
-				return {
-					tab_label(tabid, false),
-					hl = { fg = hl_tabline.fg, bg = hl_tabline.bg, style = "bold" },
-				}
-			end,
-			left_sep = {"" , hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
-			right_sep = { "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
-		},
-	}
+	hl = "TabLineFill",
+	layout = "tab_only",
+	-- head = {
+	-- 	{ "  ", hl = { fg = hl_tabline.fg, bg = hl_tabline.bg } },
+	-- 	{ "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
+	-- },
+	-- tail = {
+	-- TODO: put git stuff up here
+	-- 	{ require('gitsigns'), hl = { fg = hl_tabline.fg, bg = hl_tabline.bg } },
+	-- 	{ "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
+	-- },
+	active_tab = {
+		label = function(tabid)
+			return {
+				tab_label(tabid, true),
+				hl = { fg = hl_tabline_sel.fg, bg = hl_tabline_sel.bg, style = "bold" },
+			}
+		end,
+		left_sep = { "", hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
+		right_sep = { "", hl = { fg = hl_tabline_sel.bg, bg = hl_tabline_fill.bg } },
+	},
+	inactive_tab = {
+		label = function(tabid)
+			return {
+				tab_label(tabid, false),
+				hl = { fg = hl_tabline.fg, bg = hl_tabline.bg, style = "bold" },
+			}
+		end,
+		left_sep = { "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
+		right_sep = { "", hl = { fg = hl_tabline.bg, bg = hl_tabline_fill.bg } },
+	},
+}
 
 require("tabby").setup({
 	tabline = tabline,
