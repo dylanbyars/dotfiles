afe2c634b56cf5473844460c3a37e629f00a40e0
dylanbyars <dylan@dylanbyars.com>
2022-05-31 10:50:49 -0400

    local snippets

--- config/.config/nvim/lua/plugins/cmp.lua
+++ config/.config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,15 @@
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
 --------------------------
 -- cmp setup
 --------------------------
 vim.o.completeopt = "menuone,noselect"
-
-local cmp = require("cmp")
 
 cmp.setup({
 	snippet = {
@@ -11,20 +17,44 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = {
-		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
 	},
 	sources = {
-		{ name = "luasnip", keyword_length = 2 },
-		{ name = "buffer", keyword_length = 6 },
-		{ name = "nvim_lsp" },
 		{ name = "path" },
+		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
+		{ name = "buffer", keyword_length = 5 },
 	},
 	experimental = {
 		ghost_text = true,
