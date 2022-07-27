4a2eb438860378dcca01568198764642e0f43ae2
dylanbyars <dylan@dylanbyars.com>
2022-07-27 17:14:14 -0400

    change order of startify sections

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -238,8 +238,8 @@ vim.g.startify_change_to_vcs_root = 1
 vim.g.startify_custom_header = "startify#center(startify#fortune#cowsay())"
 vim.g.startify_lists = {
 	{ type = "sessions", header = { " Sessions" } },
-	{ type = "dir", header = { " Directory Files" } },
 	{ type = "files", header = { " Recent Files" } },
+	{ type = "dir", header = { " Directory Files" } },
 }
 
 require("plugins")
