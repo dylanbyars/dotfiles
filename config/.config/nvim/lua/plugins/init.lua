b1b3d0858414dcfeed128d6614959793604f9ff5
dylanbyars <dylan@dylanbyars.com>
2022-11-30 10:18:00 -0500

    remove unused config

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -188,16 +188,6 @@ require("fm-nvim").setup({
 
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/plugins/snippets" } })
 
--- Tree-sitter configuration
-require("nvim-treesitter.configs").setup({
-	-- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = { "org" }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-	},
-	-- ensure_installed = { "org" }, -- Or run :TSUpdate org
-})
-
 require("nvim-tree").setup({
 	view = {
 		adaptive_size = true,
