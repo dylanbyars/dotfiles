e2cf7ea016bc66f095d84f9eab9f5a65e07bd2d2
dylanbyars <dylan@dylanbyars.com>
2022-12-15 11:43:30 -0500

    no more supertab for completion selection

--- config/.config/nvim/lua/plugins/cmp.lua
+++ config/.config/nvim/lua/plugins/cmp.lua
@@ -21,53 +21,53 @@ cmp.setup({
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	-- mapping = {
-	-- 	["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-	-- 	["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-	-- 	["<esc>"] = cmp.mapping.abort(),
-	-- 	["<cr>"] = cmp.mapping(
-	-- 		cmp.mapping.confirm({
-	-- 			behavior = cmp.ConfirmBehavior.Insert,
-	-- 			select = true,
-	-- 		}),
-	-- 		{ "i", "c" }
-	-- 	),
-	-- },
 	mapping = {
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
+		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<esc>"] = cmp.mapping.abort(),
+		["<cr>"] = cmp.mapping(
+			cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = true,
+			}),
+			{ "i", "c" }
+		),
 	},
+	-- mapping = {
+	-- 	["<Tab>"] = cmp.mapping(function(fallback)
+	-- 		if cmp.visible() then
+	-- 			cmp.select_next_item()
+	-- 		elseif luasnip.expand_or_jumpable() then
+	-- 			luasnip.expand_or_jump()
+	-- 		elseif has_words_before() then
+	-- 			cmp.complete()
+	-- 		else
+	-- 			fallback()
+	-- 		end
+	-- 	end, { "i", "s" }),
+	--
+	-- 	["<S-Tab>"] = cmp.mapping(function(fallback)
+	-- 		if cmp.visible() then
+	-- 			cmp.select_prev_item()
+	-- 		elseif luasnip.jumpable(-1) then
+	-- 			luasnip.jump(-1)
+	-- 		else
+	-- 			fallback()
+	-- 		end
+	-- 	end, { "i", "s" }),
+	--
+	-- 	["<CR>"] = cmp.mapping.confirm({
+	-- 		behavior = cmp.ConfirmBehavior.Replace,
+	-- 		select = true,
+	-- 	}),
+	-- },
 	sources = {
 		{ name = "path" },
 		{ name = "luasnip" },
 		{ name = "nvim_lsp" },
 		{ name = "nvim_lua" },
 		{ name = "buffer", keyword_length = 5 },
-		{ name = 'orgmode' }
+		{ name = "orgmode" },
 	},
 	experimental = {
 		ghost_text = true,
