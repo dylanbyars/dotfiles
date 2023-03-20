2afd9797492f452dcae853613e156440106ed007
dylanbyars <dylan@dylanbyars.com>
2023-03-19 20:47:03 -0400

    cmp -> nvim-cmp

--- config/.config/nvim/lua/plugins/nvim-cmp.lua
+++ config/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,7 +1,8 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"L3MON4D3/LuaSnip", -- snippet engine
+		-- snippet engine
+		"L3MON4D3/LuaSnip",
 		-- completion sources
 		"saadparwaiz1/cmp_luasnip",
 		"hrsh7th/cmp-buffer",
@@ -9,4 +10,42 @@ return {
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-nvim-lsp",
 	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = {
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<esc>"] = cmp.mapping.abort(),
+				["<cr>"] = cmp.mapping(
+					cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+					{ "i", "c" }
+				),
+			},
+			sources = {
+				{ name = "path" },
+				{ name = "luasnip" },
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "buffer", keyword_length = 5 },
+				{ name = "orgmode" },
+			},
+			experimental = {
+				ghost_text = true,
+			},
+		})
+	end,
 }
