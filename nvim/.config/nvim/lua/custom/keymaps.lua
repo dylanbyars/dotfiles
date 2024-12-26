4a8dd283ccca349f494e71b9ee0f8d35ad9ac9da
dylanbyars <dylan@dylanbyars.com>
2024-12-26 14:59:49 -0500

    simpler keymaps

--- nvim/.config/nvim/lua/custom/keymaps.lua
+++ nvim/.config/nvim/lua/custom/keymaps.lua
@@ -1,9 +1,4 @@
---------------------------
--- general key bindings
---------------------------
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- TODO: this fails when the word is against something e.g. fooBar(baz) -> can't cycle fooBar
+-- Helper Functions
 local function cycleCase()
 	local lspRename = require("textcase").lsp_rename
 	local currentWord = require("textcase").current_word
@@ -42,81 +37,39 @@ local function open_scratch_buffer(filetype)
 	vim.cmd("set filetype=" .. filetype)
 end
 
-local KEYMAPS = {
-	["n"] = {
-		["<leader>hh"] = function()
-			vim.cmd("set cursorline!")
-		end,
-		["<leader>w"] = function()
-			vim.cmd("w")
-		end,
-		["<leader>q"] = function()
-			vim.cmd("q")
-		end,
-		["<leader>so"] = function()
-			vim.cmd("source ~/.config/nvim/init.lua")
-		end,
-		-- make splits easier
-		["<leader>\\"] = "<C-w>v",
-		["<leader>-"] = "<C-w>s",
-		-- move between tabs
-		["[t"] = function()
-			vim.cmd("tabprev")
-		end,
-		["]t"] = function()
-			vim.cmd("tabnext")
-		end,
-		-- rearrange tabs
-		["[T"] = function()
-			vim.cmd("-tabmove")
-		end,
-		["]T"] = function()
-			vim.cmd("+tabmove")
-		end,
-		-- toggle spell checking mode
-		["<leader>s"] = function()
-			vim.cmd("set spell!")
-		end,
-		-- yank selection to system clipboard
-		["<leader>y"] = '"+y',
-		-- navigate to start/end of line
-		["H"] = "^",
-		["L"] = "$",
-		["U"] = "<C-r>", -- u = undo  U = undo undo aka redo,
-		["<leader>_"] = cycleCase,
-		-- jump a half page then center the screen on the cursor's line
-		["<C-u>"] = "<C-u>zz",
-		["<C-d>"] = "<C-d>zz",
-		-- toggle inline diagnostics
-		["<leader>X"] = function()
-			vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-		end,
-		-- clear current search highlighendt
-		["<leader>n"] = function()
-			vim.cmd(":noh")
-		end,
-		["<leader>js"] = function()
-			open_scratch_buffer("json")
-		end,
-		["<leader>#"] = function()
-			vim.cmd("set relativenumber!")
-		end,
-	},
-	["v"] = {
-		-- yank selection to system clipboard
-		["<leader>y"] = '"+y',
-		-- navigate to start/end of line
-		["H"] = "^",
-		["L"] = "$",
-	},
-}
+local set = vim.keymap.set
 
-for mode, maps in pairs(KEYMAPS) do
-	for keymap, action in pairs(maps) do
-		vim.keymap.set(mode, keymap, action)
-	end
-end
+-- Shared mappings
+set({ "n", "v" }, "H", "^", { desc = "Start of line" })
+set({ "n", "v" }, "L", "$", { desc = "End of line" })
+set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+set({ "n", "v" }, "<Space>", "<Nop>", { silent = true, desc = "this is the leader key" })
 
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Normal mode mappings
+set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save buffer" })
+set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit buffer" })
+set("n", "<leader>so", "<cmd>source ~/.config/nvim/init.lua<cr>", { desc = "Source init.lua" })
+set("n", "<leader>\\", "<C-w>v", { desc = "Split vertically" })
+set("n", "<leader>-", "<C-w>s", { desc = "Split horizontally" })
+set("n", "[t", "<cmd>tabprev<cr>", { desc = "Previous tab" })
+set("n", "]t", "<cmd>tabnext<cr>", { desc = "Next tab" })
+set("n", "[T", "<cmd>-tabmove<cr>", { desc = "Move tab left" })
+set("n", "]T", "<cmd>+tabmove<cr>", { desc = "Move tab right" })
+set("n", "<leader>hh", "<cmd>set cursorline!<cr>", { desc = "Toggle cursor line" })
+set("n", "<leader>s", "<cmd>set spell!<cr>", { desc = "Toggle spell check" })
+set("n", "<leader>#", "<cmd>set relativenumber!<cr>", { desc = "Toggle relative numbers" })
+set("n", "<leader>X", function()
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "Toggle inline diagnostics" })
+set("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
+set("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+set("n", "<leader>n", "<cmd>noh<cr>", { desc = "Clear search highlight" })
+set("n", "U", "<C-r>", { desc = "Redo" })
+set("n", "<leader>_", cycleCase, { desc = "Cycle case" })
+set("n", "<leader>js", function()
+	open_scratch_buffer("json")
+end, { desc = "Open JSON scratch buffer" })
+
+-- Word wrap navigation
+set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
