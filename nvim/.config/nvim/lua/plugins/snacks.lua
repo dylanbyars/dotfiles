f0c3db9729e9ab249c591d99aec4e2fce2b41005
dylanbyars <dylan@dylanbyars.com>
2024-12-27 21:19:17 -0500

    remove comment

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,9 +3,6 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
 		bigfile = { enabled = true },
 		dashboard = {
 			enabled = true,
