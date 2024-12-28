ca1a9c716397b4ad91d57135e941c1e2cf2dbf49
dylanbyars <dylan@dylanbyars.com>
2024-12-28 14:12:08 -0500

    consolidate treesitter plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,112 +1,115 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	opts = function()
-		pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- one of "all", "maintained" (parsers with maintainers), or a list of languages
-			ensure_installed = {
-				"bash",
-				"terraform",
-				"comment", -- highlight TODO and FIXME comments
-				"css",
-				"html",
-				"javascript",
-				"python",
-				"jsdoc",
-				"markdown",
-				"markdown_inline",
-				"json",
-				"lua",
-				"regex",
-				"scss",
-				"toml",
-				"tsx",
-				"typescript",
-				"yaml",
-				"graphql",
-				"vimdoc",
-			},
-			-- highlight adds treesitter highlighting to the words in the buffer
-			highlight = { enable = true },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					-- TODO: get the decrement to work
-					node_decremental = "<c-backspace>",
+	{ "nvim-treesitter/playground", build = ":TSInstall query" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		opts = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- one of "all", "maintained" (parsers with maintainers), or a list of languages
+				ensure_installed = {
+					"bash",
+					"terraform",
+					"comment", -- highlight TODO and FIXME comments
+					"css",
+					"html",
+					"javascript",
+					"python",
+					"jsdoc",
+					"markdown",
+					"markdown_inline",
+					"json",
+					"lua",
+					"regex",
+					"scss",
+					"toml",
+					"tsx",
+					"typescript",
+					"yaml",
+					"graphql",
+					"vimdoc",
 				},
-			},
-			context_commentstring = { enable = true },
-			playground = {
-				enable = true,
-				disable = {},
-				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-				persist_queries = false, -- Whether the query persists across vim sessions
-				keybindings = {
-					toggle_query_editor = "o",
-					toggle_hl_groups = "i",
-					toggle_injected_languages = "t",
-					toggle_anonymous_nodes = "a",
-					toggle_language_display = "I",
-					focus_language = "f",
-					unfocus_language = "F",
-					update = "R",
-					goto_node = "<cr>",
-					show_help = "?",
-				},
-			},
-			textobjects = {
-				select = {
+				-- highlight adds treesitter highlighting to the words in the buffer
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
 					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						-- TODO: get the decrement to work
+						node_decremental = "<c-backspace>",
 					},
 				},
-				move = {
+				context_commentstring = { enable = true },
+				playground = {
 					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
+					disable = {},
+					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+					persist_queries = false, -- Whether the query persists across vim sessions
+					keybindings = {
+						toggle_query_editor = "o",
+						toggle_hl_groups = "i",
+						toggle_injected_languages = "t",
+						toggle_anonymous_nodes = "a",
+						toggle_language_display = "I",
+						focus_language = "f",
+						unfocus_language = "F",
+						update = "R",
+						goto_node = "<cr>",
+						show_help = "?",
 					},
 				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
 					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
 					},
 				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
